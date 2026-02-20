90c88c0236259639492fe4d2888780b2b647f116
Miguelmayorga39 <miguel.mayorga39@gmail.com>
2026-02-20 11:29:12 -0600

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,43 @@
-return (
-  <div
-    style={{
-      position: 'relative',
-      zIndex: 10,
-      textAlign: 'center',
-      marginTop: '100px',
-      color: 'white'
-    }}
-  >
-    <h1>Login Apex</h1>
-    <button onClick={login}>Entrar con Google</button>
-  </div>
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  'https://lrwlqqhchtfxpzobooqq.supabase.co',
+  'sb_publishable_04snxuCxhEl_o8vZ7hdN_g_mMOYHwn0'
 )
+
+export default function Login() {
+  const login = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google'
+    })
+  }
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 9999,
+        background: 'rgba(0,0,0,0.4)'
+      }}
+    >
+      <div
+        style={{
+          background: 'white',
+          padding: '40px',
+          borderRadius: '12px',
+          textAlign: 'center'
+        }}
+      >
+        <h1 style={{ color: 'black' }}>Login Apex</h1>
+        <button onClick={login}>Entrar con Google</button>
+      </div>
+    </div>
+  )
+}
